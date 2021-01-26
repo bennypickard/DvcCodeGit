07b40bc6e7fb1de57d08f5b0cf591120728cbe24
bennypickard <bennypickard@gmail.com>
2021-01-26 11:16:33 -0800

    test project

---
+++ COMSC-255/as1/src/as1/Message.java
@@ -0,0 +1,14 @@
+package as1;
+
+import javax.swing.JOptionPane;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String msg;
+		msg = "Hi\n";
+		JOptionPane.showMessageDialog(null, msg);
+	}
+
+}
