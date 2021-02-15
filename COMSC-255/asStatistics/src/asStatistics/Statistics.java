6f26408a3570fafa2c2ed7e03b271124c6f93ee6
bennypickard <bennypickard@gmail.com>
2021-02-15 15:01:51 -0800

    Did assignment 7
    Modified assignment 6 (asStatistics) to complete assigment 7.

--- COMSC-255/asStatistics/src/asStatistics/Statistics.java
+++ COMSC-255/asStatistics/src/asStatistics/Statistics.java
@@ -1,5 +1,87 @@
 package asStatistics;
 
-public class Statistics {
+import java.util.Arrays;
 
+
+public class Statistics {
+	//This function has some arrays and does some operations to them
+	
+	//instance variables
+	private double [] data;
+	private double [] sdata;
+	
+	//constructor
+	public Statistics(double [] d)
+	{
+		//clone the array right away
+		data = d.clone();
+		sdata = d.clone();
+		
+		//sort sdata array
+		Arrays.sort(sdata);
+	}
+	
+	//instance methods
+	public double findMin()
+	{
+		//sdata is from least to greatest. 1st position is minimum
+		return sdata[0];
+	}
+	
+	public double findMax()
+	{
+		//sdata is least to greatest, last index is greatest
+		return sdata[sdata.length-1];
+	}
+	
+	public double findMean()
+	{
+		double sum = 0, mean = 0;
+		//Sum all the values
+		for( int i = 0; i < sdata.length; i++)
+		{
+			sum += sdata[i];
+		}
+		//Divide by length to get mean
+		mean = sum / sdata.length;
+		
+		return mean;
+	}
+	
+	public double findMedian()
+	{
+		//variables
+		int index, indexHi, indexLo;
+		double median;
+		//find if odd or even
+		if(sdata.length % 2 != 0)
+		{//if odd, median is the one in the middle
+			index = sdata.length / 2;
+			median = sdata[index];
+		}
+		else
+		{//take an average of 2 middle numbers
+			indexHi = sdata.length / 2; //Rounds up?
+			indexLo = indexHi -1;
+			median = (sdata[indexLo] + sdata[indexHi]) / 2;
+		}
+		return median;
+	}
+	
+	public double [] getOrigData()
+	{
+		//create a new array to return
+		double [] d = data.clone();
+		return d;
+	}
+	
+	public double [] getSortedData()
+	{
+		//create new array to return
+		double [] d = sdata.clone();
+		return d;
+	}
+	
+	
+	
 }
--- COMSC-255/asStatistics/src/asStatistics/TestStatistics.java
+++ COMSC-255/asStatistics/src/asStatistics/TestStatistics.java
@@ -1,10 +1,133 @@
 package asStatistics;
 
+import java.text.DecimalFormat;
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
+
+
 public class TestStatistics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// This method tests the Statistics class and calls each of its functions on an array
 
+		//[][]Variables
+		String in, out, delim, token;
+		int itemCount = 0;
+		double min = 0, max = 0, mean = 0, median = 0;
+		int placesCount;
+		
+		/*
+		[][][]OLD INPUT CODE[][][]
+		//input item count
+		in = JOptionPane.showInputDialog("Enter the Number Of Data Values: ");
+		itemCount = Integer.parseInt(in);//turn to int
+		
+		//new array
+		double [] data = new double [itemCount];
+		
+		//input data into array
+		for(int i = 0 ; i < itemCount ; i ++)
+		{
+			in = JOptionPane.showInputDialog("Enter Data Value");
+			data[i] = Double.parseDouble(in);//turn to double
+		}
+		
+		*/
+		
+		
+		//[][][]TOKENIZER CODE[][][]
+		delim = ",";
+		
+		//INPUT STRING
+		in = JOptionPane.showInputDialog("Enter data values separated by commas");
+		
+		
+		//create new Tokenizer object
+		StringTokenizer st = new StringTokenizer(in, delim);//instantly tokenizes the string into an array
+		
+		//Get array length.
+		itemCount = st.countTokens();
+		
+		//Create data array with itemCount length
+		double [] data = new double [itemCount];
+		
+		//This for loop converts the Tokens in st into a usable data array
+		for(int i = 0 ; i < itemCount ; i ++)
+		{
+			token = st.nextToken();//fetch a string from the array
+			//TRIM the token
+			token = token.trim();
+			
+			//add String to data array.
+			data[i] = Double.parseDouble(token);
+		}
+		
+		
+		
+		
+		
+		//[][][]STATISTICS CLASS CODE[][][]
+		//create Statistics object to handle array
+		Statistics stat = new Statistics(data);
+		
+		//find min, max, median, with methods
+		min = stat.findMin();
+		max = stat.findMax();
+		mean = stat.findMean();
+		median = stat.findMedian();
+		
+		//retrieve data back from function
+		double [] origData = stat.getOrigData();
+		double [] sortData = stat.getSortedData();
+		
+		//[][][]DECIMAL PLACE CODE
+		//input how many decimal places we want.
+		in = JOptionPane.showInputDialog("Enter number of decimal places for output");
+		placesCount = Integer.parseInt(in);
+		
+		String pattern = "0.";
+		for ( int i = 0 ; i < placesCount ; i ++)
+		{//append zeros till satisfied
+			pattern = pattern + "0";
+		}
+		
+		//create DecimalFormat object and give it the pattern
+		DecimalFormat df = new DecimalFormat();
+		df.applyPattern(pattern);
+		
+		
+		
+
+		//[][]START OUTPUT STATEMENT
+		
+		//Build output statement
+        out = "Original Data: \n";
+        for (int i = 0 ; i < origData.length ; i ++)
+        {//loop thru original data list.
+        	out = out + origData [i] + " ";
+        }
+        out += "\n";//newline
+
+        //Add sorted data to output
+        out = out + "Sorted Data: \n";
+        for (int i = 0 ; i < sortData.length ; i ++)
+        {//loop thru sorted data list.
+        	out = out + sortData [i] + " ";
+        }
+        out += "\n\nComputed Values Displayed to " + placesCount + " Decimal Places\n";//Title Line
+        
+        //value output
+        out += "Min Value: " + df.format(min) + "\n";
+        out += "Max Value: " + df.format(max) + "\n";
+        out += "Mean: " + df.format(mean) + "\n";
+        out += "Median: " + df.format(median) + "\n";
+
+        
+		//Deliver output
+        JOptionPane.showMessageDialog ( null, out);
+        
+		
+		
 	}
 
 }
