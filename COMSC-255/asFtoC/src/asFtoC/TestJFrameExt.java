4989ca4c8b457d4db27cba9c84cafaaff8a03622
bennypickard <bennypickard@gmail.com>
2021-04-24 13:20:00 -0700

    as 17

---
+++ COMSC-255/asFtoC/src/asFtoC/TestJFrameExt.java
@@ -0,0 +1,23 @@
+package asFtoC;
+
+import asFtoC.JFrameExt;
+
+public class TestJFrameExt {
+
+	
+
+	public static void main(String[] args) {
+		 JFrameExt f = new JFrameExt();
+		
+		 f.setSize(400, 300);
+		 
+		 f.setVisible(true);
+		
+	}
+	
+	
+}
+
+
+
+
